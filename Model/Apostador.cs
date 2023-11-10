4cc224414b571550e829310e8a95be2ff7f94434
RianAbdias <rianabdias123@gmail.com>
2023-11-10 07:11:12 -0300

    arrumando o model do apostador

--- Model/Apostador.cs
+++ Model/Apostador.cs
@@ -7,6 +7,6 @@ namespace apiBambets.Model
         public int Id {get; set;}
         public string? Nome { get; set; }
         public int Numero { get; set; }
-        public int Palpite { get; set; }
+        public string? Palpite { get; set; }
     }
 }
