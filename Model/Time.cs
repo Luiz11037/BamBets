26c8933cc6e54ed0535956cdb9bfd680b9f49cfd
RianAbdias <rianabdias123@gmail.com>
2023-11-27 09:13:09 -0300

    criação do model de time

---
+++ Model/Time.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace apiBambets.Model
+{
+    public class Time
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+    //Adicionar classe(model) time, adicionar um apostador e um jogo//
+    }
+}
