162da28238a51ef73647bc193939fe499fa6ff50
Luiz11037 <luizfelipe31felipe@gmail.com>
2023-12-26 11:19:15 -0300

    ...

--- Context/apiBambetsContext.cs
+++ Context/apiBambetsContext.cs
@@ -4,15 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using apiBambets.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace apiBambets.Context;
 
-public class apiBambetsContext : DbContext
+public class apiBambetsContext : IdentityDbContext
 {
     public apiBambetsContext(DbContextOptions options) : base(options) {}
 
     public DbSet<Aposta>? Apostas {get; set;}
-
     public DbSet<Apostador>? Apostadores {get; set;}
     public DbSet<Jogo>? Jogos {get; set;}
     public DbSet<Time>? Times {get; set;}
--- Program.cs
+++ Program.cs
@@ -44,11 +44,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.Run();
